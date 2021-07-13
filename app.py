ba360290d83f93f575060fae9c590e7d96b5c27d
Vickie Hughes <vickie_hughes@hotmail.com>
2021-07-13 18:45:37 -0500

    Update

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 import os
 import sqlite3 as sql
+import sys 
 
 #init app and class
 app = Flask(__name__)
@@ -28,8 +29,37 @@ def getData():
 ####################################
 # ADD MORE ENDPOINTS
 ###########################################
+<<<<<<< Updated upstream
 #approute for bar chart
 @app.route("/barChart", methods=["GET"])
+=======
+#app route for boxplot
+@app.route("/box", methods=["GET"])
+def boxPlot():
+    moddedDF = df[["age_youngest","num_injured","gender","year"]]
+    agegroup = moddedDF[["age_youngest","num_injured","gender","year"]]
+    bins= [0,1,11,21,31,41,51,61,200]
+    labels = ['0','01-10','11-20','21-30','31-40','41-50','51-60','60+']
+    agegroup['AgeGroup'] = pd.cut(agegroup['age_youngest'], bins=bins, labels=labels, right=False)
+    boxchartdf = agegroup.groupby(['AgeGroup','gender']).num_injured.sum().reset_index()
+    boxData = []
+    for index, row in boxchartdf:
+        box_plot = {'age_group': row['AgeGroup'],
+                    'gender': row['gender'],
+                    'numInjured': row['num_injured']}
+        boxData.append(box_plot)
+
+    stackdata = []
+    accidents_byage_bygender = boxchartdf.groupby("AgeGroup")
+    for index, row in accidents_byage_bygender: 
+        e = {"AgeGroup":row["AgeGroup"], "Maccidents":row["M"],"Faccidents":row["F"],"Uaccidents":row["U"]}
+    print(accidents_byage_bygender.groups, file = sys.stderr) 
+    # return jsonify(boxData)
+    return 'ok'
+
+#app route for bar chart
+@app.route("/bar", methods=["GET"])
+>>>>>>> Stashed changes
 def barChart():
 
     return barData
