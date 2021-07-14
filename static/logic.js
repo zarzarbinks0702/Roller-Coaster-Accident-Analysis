c23ce9e919fa762a8800f4d617db8781690d09c5
Vickie Hughes <vickie_hughes@hotmail.com>
2021-07-13 22:22:17 -0500

    updated boxchart

--- static/logic.js
+++ static/logic.js
@@ -93,68 +93,33 @@ function createScatter() {
 createScatter();
 
 function createBar() {
-    d3.json("/bar").then((data) => {
-        console.log(data)
-        var stackdata = []
-
-for (const entry of data){
-
-}
-
-
-// Define SVG area dimensions
-var svgWidth = 960;
-var svgHeight = 660;
-
-// Define the chart's margins as an object
-var chartMargin = {
-  top: 30,
-  right: 30,
-  bottom: 30,
-  left: 30
-};
-
-// Define dimensions of the chart area
-var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
-var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
-
-// Select body, append SVG area to it, and set the dimensions
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("height", svgHeight)
-  .attr("width", svgWidth);
-
-// Append a group to the SVG area and shift ('translate') it to the right and down to adhere
-// to the margins set in the "chartMargin" object.
-var chartGroup = svg.append("g")
-  .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
-
-  // Cast the hours value to a number for each piece of tvData
-  data.forEach(function(d) {
-    d.numInjured = +d.numInjured;
-  });
-
-  var barSpacing = 10; // desired space between each bar
-  var scaleY = 10; // 10x scale on rect height
-
-  // Create a 'barWidth' variable so that the bar chart spans the entire chartWidth.
-  var barWidth = (chartWidth - (barSpacing * (data.length - 1))) / data.length;
-
-  // @TODO
-  // Create code to build the bar chart using the tvData.
-  chartGroup.selectAll(".bar")
-    .data(data)
-    .enter()
-    .append("rect")
-    .classed("bar", true)
-    .attr("width", d => barWidth)
-    .attr("height", d => d.numInjured * scaleY)
-    .attr("x", (d, i) => i * (barWidth + barSpacing))
-    .attr("y", d => chartHeight - d.numInjured * scaleY);
-
-    });
-}
+  d3.json("/bar").then((data) => {
+      console.log(data)
+      var gender = [];
+      var injuries = [];
+  
+      for (const entry of data) {
+        let g 
+        if(entry.gender=='F') g='Female'
+        if(entry.gender=='M') g='Male'
+        if(entry.gender=='U') g='Unknown'
+          gender.push(g);
+          injuries.push(entry.numInjured);
+      }
+  
+    var trace1 = {
+      x: gender,
+      y: injuries,
+      name: 'Accidents by Gender',
+      type: 'bar'
+    };
+    
+    var data1 = [trace1];
+    
+    Plotly.newPlot('bar', data1);
+  
+      });
+  }
 createBar();
 
 function createPie() {
